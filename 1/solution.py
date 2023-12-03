e97f74edfa428a8b19dcd0565169d3082eaba719
sduemelr <samduemler@gmail.com>
2023-12-03 14:43:44 -0500

    Day 1 Part 2

--- 1/solution.py
+++ 1/solution.py
@@ -2,7 +2,6 @@ input = open("1/input.txt", "r")
 lines = input.readlines()
 
 totalValue = 0
-numLines = 0
 
 for line in lines:
   lineValue = ''
@@ -17,3 +16,49 @@ for line in lines:
   totalValue = totalValue + int(lineValue)
 
 print(totalValue)
+
+totalValue = 0
+numMap = {'1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six', '7':'seven', '8':'eight', '9':'nine'}
+keyList = list(numMap.keys())
+valueList = list(numMap.values())
+
+for line in lines:
+  #double for loop, for each character go five out and check if it's in the dictionary
+  #then the same thing but backards, adding to the front of the tempValue
+
+  lineValue = ''
+  x = 0
+  while lineValue == '' and x < len(line):
+    if line[x].isdigit():
+      lineValue += line[x]
+      break
+    else:
+      tempValue = line[x]
+      i = x
+      while i+1 != len(line) and not len(tempValue) > 5:
+        i += 1
+        tempValue += line[i]
+        if tempValue in valueList:
+          lineValue += keyList[valueList.index(tempValue)]
+          break
+    x += 1
+
+  x = len(line) - 1
+  while len(lineValue) == 1 and x != -1:
+    if line[x].isdigit():
+      lineValue += line[x]
+      break
+    else:
+      tempValue = line[x]
+      i = x
+      while i-1 != -1 and not len(tempValue) > 5:
+        i -= 1
+        tempValue = line[i] + tempValue
+        if tempValue in valueList:
+          lineValue += keyList[valueList.index(tempValue)]
+          break
+    x -= 1
+  totalValue += int(lineValue)
+
+print(totalValue)
+
