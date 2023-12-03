933a8d0235a5b4ddf309a6944fd12a4f7f666aa9
sduemler <samduemler@gmail.com>
2023-12-03 16:49:26 -0500

    Day 2 Part 1

---
+++ 2/solution.py
@@ -0,0 +1,28 @@
+input = open("2/input.txt", "r")
+lines = input.readlines()
+
+gameMap = {}
+totalIds = 0
+
+for line in lines:
+  line = line.rstrip()
+  maxRed = 0
+  maxGreen = 0
+  maxBlue = 0
+  firstSplit = line.split(': ')
+  id = firstSplit[0].split(' ')[1]
+  gameSplit = firstSplit[1].split('; ')
+  for split in gameSplit:
+    pullSplit = split.split(', ')
+    for pull in pullSplit:
+      infoSplit = pull.split(' ')
+      if infoSplit[1] == 'red' and int(infoSplit[0]) > maxRed:
+        maxRed = int(infoSplit[0])
+      if infoSplit[1] == 'blue' and int(infoSplit[0]) > maxBlue:
+        maxBlue = int(infoSplit[0])
+      if infoSplit[1] == 'green' and int(infoSplit[0]) > maxGreen:
+        maxGreen = int(infoSplit[0])
+  gameMap[id] = [maxRed, maxBlue, maxGreen]
+  if (maxRed < 13 and maxGreen < 14) and maxBlue < 15:
+    totalIds += int(id)
+print(totalIds)
