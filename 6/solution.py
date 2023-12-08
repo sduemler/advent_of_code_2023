3d1237790de2f8bf129b63b6be513a286fc12bc2
sduemler <samduemler@gmail.com>
2023-12-08 10:36:59 -0500

    Day 6 Part 2

--- 6/solution.py
+++ 6/solution.py
@@ -2,7 +2,9 @@ input = open("6/input.txt", "r")
 lines = input.readlines()
 
 times = []
+oneTime = 0
 distances = []
+oneDistance = 0
 recordMultiply = 1
 
 for line in lines:
@@ -10,8 +12,11 @@ for line in lines:
     raceSplit = line.split(':')
     if raceSplit[0] == 'Time':
         times = ' '.join(raceSplit[1].split(" ")).split()
+        oneTime = int(''.join(times))
+        print(oneTime)
     if raceSplit[0] == 'Distance':
         distances = ' '.join(raceSplit[1].split(' ')).split()    
+        oneDistance = int(''.join(distances))
 
 for x in range(len(times)):
     raceTimes = 0
@@ -21,5 +26,12 @@ for x in range(len(times)):
             raceTimes += 1
     recordMultiply = recordMultiply * raceTimes
 
+raceTimes = 0
+for x in range(oneTime):
+    raceTime = x * (oneTime - x)
+    if raceTime > oneDistance:
+        raceTimes += 1
+
 print(recordMultiply)
+print(raceTimes)
             
